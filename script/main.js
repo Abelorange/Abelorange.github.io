1b19cbdd1e5b32f84fbbe8cd0365f036c20f68eb
abelorange <abelorange@foxmail.com>
2021-01-31 14:19:38 +0800

    a

--- script/main.js
+++ script/main.js
@@ -9,7 +9,7 @@
 $(document).ready(function() {
 	$("#count").countdown({
 		//to change lunch date just replace the current date with yours .
-		date: "July 6, 2019",  
+		date: "June 30, 2021",  
 		//html code in count div here.
 		htmlTemplate: "<div id='days-count' class='numbers'>%{d}<span class='days-label'>days</span></div> <div id='hours-count' class='numbers'>%{h}<span class='hours-label'>hours</span></div><div id='min-count' class='numbers'>%{m}<span class='min-label'>min</span></div><div id='sec-count' class='numbers'>%{s}<span class='sec-label'>sec</span></div>"
 	});
